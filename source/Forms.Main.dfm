32be5e0306c958b75ea2e160ab3fb0b5d1306547
wlandgraf <10242580+wlandgraf@users.noreply.github.com>
2026-08-25 10:40:56 -0300

    First version with Spring4D dependency

---
+++ source/Forms.Main.dfm
@@ -0,0 +1,24 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 495
+  ClientWidth = 694
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  OnCreate = FormCreate
+  TextHeight = 15
+  object Memo1: TMemo
+    Left = 184
+    Top = 192
+    Width = 305
+    Height = 201
+    Lines.Strings = (
+      'Memo1')
+    TabOrder = 0
+  end
+end
---
+++ source/Forms.Main.pas
@@ -0,0 +1,39 @@
+unit Forms.Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+uses
+  Spring;
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  Items: Shared<TStringList>;
+begin
+  // Shared<T> usage (automatic reference counting/freeing)
+  Items := TStringList.Create;
+  Items.Add('Smart Setup');
+  Memo1.Lines.Add('List content: ', Items[0]);
+end;
+
+end.
