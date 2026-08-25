df1613b69d0b0f99766824002854a3e4ad9c63df
wlandgraf <10242580+wlandgraf@users.noreply.github.com>
2026-08-25 10:56:26 -0300

    Added VirtualTreeView

--- source/Forms.Main.dfm
+++ source/Forms.Main.dfm
@@ -13,12 +13,27 @@ object Form1: TForm1
   OnCreate = FormCreate
   TextHeight = 15
   object Memo1: TMemo
-    Left = 184
-    Top = 192
+    Left = 264
+    Top = 208
     Width = 305
     Height = 201
     Lines.Strings = (
       'Memo1')
     TabOrder = 0
+  end
+  object VirtualStringTree1: TVirtualStringTree
+    Left = 32
+    Top = 40
+    Width = 281
+    Height = 153
+    Header.AutoSizeIndex = 0
+    Header.Height = 15
+    Header.MainColumn = -1
+    TabOrder = 1
+    OnGetText = VirtualStringTree1GetText
+    OnInitNode = VirtualStringTree1InitNode
+    Touch.InteractiveGestures = [igPan, igPressAndTap]
+    Touch.InteractiveGestureOptions = [igoPanSingleFingerHorizontal, igoPanSingleFingerVertical, igoPanInertia, igoPanGutter, igoParentPassthrough]
+    Columns = <>
   end
 end
--- source/Forms.Main.pas
+++ source/Forms.Main.pas
@@ -4,12 +4,25 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls,
+  VirtualTrees.BaseAncestorVCL, VirtualTrees.BaseTree, VirtualTrees.AncestorVCL,
+  VirtualTrees;
 
 type
+  PMyData = ^TMyData;
+  TMyData = record
+    Caption: string;
+  end;
+
   TForm1 = class(TForm)
     Memo1: TMemo;
+    VirtualStringTree1: TVirtualStringTree;
     procedure FormCreate(Sender: TObject);
+    procedure VirtualStringTree1GetText(Sender: TBaseVirtualTree;
+      Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
+      var CellText: string);
+    procedure VirtualStringTree1InitNode(Sender: TBaseVirtualTree; ParentNode,
+      Node: PVirtualNode; var InitialStates: TVirtualNodeInitStates);
   private
     { Private declarations }
   public
@@ -27,12 +40,32 @@ uses
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
-var
-  Items: Shared<TStringList>;
 begin
+  // Spring4D
+  var Items: Shared<TStringList>;
   Items := TStringList.Create;
   Items.Value.Add('Smart Setup');
   Memo1.Lines.Add('List content: ' + Items.Value[0]);
+
+  // VirtualTreeView
+  VirtualStringTree1.NodeDataSize := SizeOf(TMyData);
+  VirtualStringTree1.RootNodeCount := 5;
+end;
+
+procedure TForm1.VirtualStringTree1GetText(Sender: TBaseVirtualTree;
+  Node: PVirtualNode; Column: TColumnIndex; TextType: TVSTTextType;
+  var CellText: string);
+begin
+  var Data: PMyData := Sender.GetNodeData(Node);
+  if Assigned(Data) then
+    CellText := Data.Caption;
+end;
+
+procedure TForm1.VirtualStringTree1InitNode(Sender: TBaseVirtualTree;
+  ParentNode, Node: PVirtualNode; var InitialStates: TVirtualNodeInitStates);
+begin
+  var Data: PMyData := Sender.GetNodeData(Node);
+  Data.Caption := 'Item ' + IntToStr(Node.Index + 1);
 end;
 
 end.
